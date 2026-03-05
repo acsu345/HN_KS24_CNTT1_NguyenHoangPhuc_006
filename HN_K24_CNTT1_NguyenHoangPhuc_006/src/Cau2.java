6c3936333e47c9ce1d4d951c9c5fd08d629a9511
acsu345 <phuchb24@gmail.com>
2026-03-05 15:31:58 +0700

    Add files via upload

--- HN_K24_CNTT1_NguyenHoangPhuc_006/src/Cau2.java
+++ HN_K24_CNTT1_NguyenHoangPhuc_006/src/Cau2.java
@@ -3,13 +3,16 @@ import java.util.*;
 public class Cau2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        //Nhập số lượng tài liệu
         System.out .println("Nhập số lượng tài liệu: ");
         int n = sc.nextInt();
         List<String> taiLieu = new ArrayList<>();
         Set<String> trungLap = new HashSet<>();
+        //Kiểm tra điều kiện
         if(n<=0){
             System.out.println("Số lượng tài liệu không hợp lệ");
         }else{
+            //Nhập tài liệu vào list
             for(int i = 0; i < n; i++){
                 System.out.println("Nhập tên tài liệu: ");
                 String str = sc.next();
@@ -17,7 +20,7 @@ public class Cau2 {
             }
         }
 
-
+        //Hiển thị danh sách ban đầu
         System.out.println("Danh sách ban đầu: " + taiLieu);
 
     }
